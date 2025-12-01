2a47fbc4d40ebcfbc7827de3826d15340dc97eb7
sijilliapp <sijilliapp@gmail.com>
2025-12-01 13:06:45 +0300

    Fix logo display issue in web version by changing LogoVariant.hd to LogoVariant.normal in register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -94,7 +94,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   AppLogo(
                     width: 100,
                     height: 100,
-                    variant: LogoVariant.hd,
+                    variant: LogoVariant.normal,
                     useHighQuality: true,
                   ),
                   const SizedBox(height: 16),
